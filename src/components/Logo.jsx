2cea3790b024357bbb97903dc5f97299e3a77e44
aherreramilet <aherrera@dl.cl>
2024-03-19 17:47:12 -0300

    actualizando componente Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,9 @@
-
 import React from 'react';
+import logo1 from "../assets/logo.jpeg";
 
 const Logo = () => {
   const isProduction = import.meta.env.NODE_ENV === 'production';
+  console.log("ESte es el valor de isProduction", isProduction);
   const logoSrc = isProduction
     ? 'https://calpesocorporal.vercel.app/logo.jpeg'
     : '/src/assets/logo.jpeg';
