c2f3bf1cbae23a0cd9ece49aa56db9d9ea0051f2
aherreramilet <aherrera@dl.cl>
2024-03-19 18:04:20 -0300

    fix componente logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo1 from "../assets/logo.jpeg";
 
 const Logo = () => {
-  const isProduction = import.meta.env.NODE_ENV === 'production';
+  /* const isProduction = import.meta.env.NODE_ENV === 'production';
   console.log("ESte es el valor de isProduction", isProduction);
   const logoSrc = isProduction
     ? 'https://calpesocorporal.vercel.app/logo.jpeg'
-    : '/src/assets/logo.jpeg';
+    : '/src/assets/logo.jpeg'; */
 
-  return <img src={logoSrc} alt="Logo" className="h-8" />;
+  return <img src={logo1} alt="Logo" className="h-8" />;
 };
 
 export default Logo;
